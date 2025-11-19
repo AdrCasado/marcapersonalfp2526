678f12e34364182e532f38627db582a06758eeac
Adrian_C <3341802@alu.murciaeduca.es>
2025-11-19 09:49:59 +0100

    trabajar controladores

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -2,7 +2,26 @@
 
 @section('content')
 
-    Vista detalle proyecto {{$id}}
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+
+            <a href="#"><img src="{{ asset('images/mp-logo.png') }}" alt="Logo-proyecto" style="height: 400px"></a>
+
+        </div>
+        <div class="col-sm-8">
+
+            <header>
+                <h3>{{ $proyecto['nombre'] }}</h3>
+            </header>
+            <p>ID del docente: {{ $proyecto['docente_id'] }}</p>
+            <p>
+                Dominio: <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
+                    http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+                </a>
+            </p>
+        </div>
+    </div>
 
 @stop
 
