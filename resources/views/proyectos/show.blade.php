510b429ba59d1c75c6a8c68dd563f96e9801b040
Adrian_C <3341802@alu.murciaeduca.es>
2025-12-02 09:53:50 +0100

    rama de ejercicios de base de datos laravel

--- resources/views/proyectos/show.blade.php
+++ resources/views/proyectos/show.blade.php
@@ -12,16 +12,16 @@
         </div>
         <div class="col-sm-8">
 
-            <h3><strong>Nombre: </strong>{{ $proyecto['nombre'] }}</h3>
+            <h3><strong>Nombre: </strong>{{ $proyecto->nombre }}</h3>
             <h4><strong>Dominio: </strong>
-                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}">
-                    http://github.com/2DAW-CarlosIII/{{ $proyecto['dominio'] }}
+                <a href="http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}">
+                    http://github.com/2DAW-CarlosIII/{{ $proyecto->dominio }}
                 </a>
             </h4>
-            <h4><strong>Docente: </strong>{{ $proyecto['docente_id'] }}</h4>
+            <h4><strong>Docente: </strong>{{ $proyecto->docente_id }}</h4>
             <p><strong>Metadatos: </strong>
                 <ul>
-                    @foreach ($proyecto['metadatos'] as $indice => $metadato)
+                    @foreach ($proyecto->metadatos as $indice => $metadato)
                         <li>{{ $indice }}: {{ $metadato }}</li>
                     @endforeach
                 </ul>
